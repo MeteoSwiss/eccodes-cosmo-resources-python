1dd33cc508a0a38440e2981923aee0f560fd7e7d
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2025-04-08 15:45:15 +0200

    update definitions from tarfile (#3)
    - remove submodule
    - add tar file of the definitions
    - bump version to 2.38.3.1

--- hatch_build.py
+++ hatch_build.py
@@ -1,9 +1,42 @@
 """Custom build hook to dereference symlinks in the defintions."""
 
 import shutil
-from typing import Any
+import tarfile
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Any, Callable
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+def common_root(names: list[str], abort: Callable[[str], None]) -> str:
+    common = None
+    for name in names:
+        if common is None:
+            common, *_ = Path(name).parts
+            continue
+        current, *_ = Path(name).parts
+        if current != common:
+            abort("tarfile has non matching root in names")
+    return common
+
+
+@contextmanager
+def unpack_definitions(abort: Callable[[str], None]) -> str:
+    if (root := Path("eccodes-cosmo-resources")).exists():
+        yield str(root / "definitions")
+        return
+
+    [path] = Path(".").glob("eccodes_definitions*.tar.bz2")
+    with tarfile.open(path, "r:bz2") as tar:
+        root = common_root(tar.getnames(), abort)
+        if not root.startswith("definitions"):
+            abort("tarfile root does not start with definitions")
+        tar.extractall(filter="data")
+    try:
+        yield root
+    finally:
+        shutil.rmtree(root)
 
 
 class CustomBuildHook(BuildHookInterface):
@@ -12,12 +45,13 @@ class CustomBuildHook(BuildHookInterface):
         version: str,
         build_data: dict[str, Any],
     ) -> None:
-        shutil.copytree(
-            "eccodes-cosmo-resources",
-            "tmp",
-            symlinks=False,
-            dirs_exist_ok=True,
-        )
+        with unpack_definitions(self.app.abort) as root:
+            shutil.copytree(
+                root,
+                "tmp",
+                symlinks=False,
+                dirs_exist_ok=True,
+            )
 
     def finalize(
         self,
