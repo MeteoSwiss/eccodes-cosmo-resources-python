89efa5f252bb7854f4eb39b880db9938a255cb31
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2025-04-08 11:54:54 +0200

    update definitions from tarfile

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ vendor = eccodes.codes_definition_path()
 cosmo = eccodes_cosmo_resources.get_definitions_path()
 eccodes.set_definition_path(f"{cosmo}:{vendor}")
 ```
+
+## Credits
+
+The eccodes_definitions.edzw on https://opendata.dwd.de are provided under the CC BY 4.0 license (https://creativecommons.org/licenses/by/4.0/) and the attribution rules apply if you use it (e.g. https://wiki.creativecommons.org/wiki/License_Versions#Attribution_reasonable_to_means.2C_medium.2C_and_context ); the owner is the DWD.
--- hatch_build.py
+++ hatch_build.py
@@ -1,9 +1,23 @@
 """Custom build hook to dereference symlinks in the defintions."""
 
 import shutil
+import tarfile
+from pathlib import Path
 from typing import Any
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+def common_root(names: list[str]):
+    common = None
+    for name in names:
+        if common is None:
+            common, *_ = Path(name).parts
+            continue
+        current, *_ = Path(name).parts
+        if current != common:
+            raise ValueError("non matching root")
+    return common
 
 
 class CustomBuildHook(BuildHookInterface):
@@ -12,12 +26,19 @@ class CustomBuildHook(BuildHookInterface):
         version: str,
         build_data: dict[str, Any],
     ) -> None:
+        [path] = Path(".").glob("eccodes_definitions*.tar.bz2")
+        with tarfile.open(path, "r:bz2") as tar:
+            root = common_root(tar.getnames())
+            if not root.startswith("definitions"):
+                raise RuntimeError
+            tar.extractall(filter="data")
         shutil.copytree(
-            "eccodes-cosmo-resources",
+            root,
             "tmp",
             symlinks=False,
             dirs_exist_ok=True,
         )
+        shutil.rmtree(root)
 
     def finalize(
         self,
